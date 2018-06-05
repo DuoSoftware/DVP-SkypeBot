f9d9582d58c02d9a6e00a76c98401983ebeeff5a
sine19 <sineth17@gmail.com>
2018-06-05 16:47:14 +0530

    clear all values
    remove all default.js and conf.js values

--- config/default.js
+++ config/default.js
@@ -11,25 +11,25 @@ module.exports = {
         "secret": "abcdefgh",
         "company": 103,
         "tenant": 1,
-        "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdWtpdGhhIiwianRpIjoiYWEzOGRmZWYtNDFhOC00MWUyLTgwMzktOTJjZTY0YjM4ZDFmIiwic3ViIjoiNTZhOWU3NTlmYjA3MTkwN2EwMDAwMDAxMjVkOWU4MGI1YzdjNGY5ODQ2NmY5MjExNzk2ZWJmNDMiLCJleHAiOjE5MDIzODExMTgsInRlbmFudCI6LTEsImNvbXBhbnkiOi0xLCJzY29wZSI6W3sicmVzb3VyY2UiOiJhbGwiLCJhY3Rpb25zIjoiYWxsIn1dLCJpYXQiOjE0NzAzODExMTh9.Gmlu00Uj66Fzts-w6qEwNUz46XYGzE8wHUhAJOFtiRo"
+        "token": ""
 
 
     },
 
     "Bot":{
-        "appId": "4d973783-5aea-427b-bdf7-28e48fab1e16",
-        "appPassword": "pb8KyMaO7YV0j8omHZ9yfdy"
+        "appId": "",
+        "appPassword": ""
     },
 
 
     "Services": {
 
-        "messengerhost": "externalipmessagingservice.app.veery.cloud",//ardsliteservice.app.veery.cloud
+        "messengerhost": "",//ardsliteservice.app.veery.cloud
         "messengerport": "3334",
         "messengerversion": "1.0.0.0",
         //externalipmessagingservice.app.veery.cloud
 
-        "csaturl": "csatservice.app.veery.cloud",
+        "csaturl": "",
         //csatservice.app.veery.cloud
         "csatport": '4445',
         "csatversion": "1.0.0.0"
